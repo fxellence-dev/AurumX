371e78ddf6333b1abb0be8b8278998a75bbbac8b
amitmahajan78 <amitmahajan@post.com>
2025-11-15 15:02:45 +0000

    updated live rate widget

--- src/components/TradingViewWidget.tsx
+++ src/components/TradingViewWidget.tsx
@@ -57,19 +57,54 @@ export const TradingViewWidget: React.FC<TradingViewWidgetProps> = ({
 <body>
   <div class="tradingview-widget-container">
     <div class="tradingview-widget-container__widget"></div>
-    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-mini-symbol-overview.js" async>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js" async>
     {
-      "symbol": "${symbol}",
-      "width": "100%",
-      "height": "100%",
-      "locale": "en",
-      "dateRange": "12M",
+      "lineWidth": 2,
+      "lineType": 0,
+      "chartType": "area",
+      "fontColor": "rgb(106, 109, 120)",
+      "gridLineColor": "rgba(46, 46, 46, 0.06)",
+      "volumeUpColor": "rgba(34, 171, 148, 0.5)",
+      "volumeDownColor": "rgba(247, 82, 95, 0.5)",
+      "backgroundColor": "#0A0A0A",
+      "widgetFontColor": "#FFFFFF",
+      "upColor": "#22ab94",
+      "downColor": "#f7525f",
+      "borderUpColor": "#22ab94",
+      "borderDownColor": "#f7525f",
+      "wickUpColor": "#22ab94",
+      "wickDownColor": "#f7525f",
       "colorTheme": "dark",
       "isTransparent": false,
+      "locale": "en",
+      "chartOnly": false,
+      "scalePosition": "right",
+      "scaleMode": "Normal",
+      "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+      "valuesTracking": "1",
+      "changeMode": "price-and-percent",
+      "symbols": [
+        [
+          "${symbol}|1D"
+        ]
+      ],
+      "dateRanges": [
+        "1d|1",
+        "1m|30",
+        "3m|60",
+        "12m|1D",
+        "60m|1W",
+        "all|1M"
+      ],
+      "fontSize": "10",
+      "headerFontSize": "small",
       "autosize": true,
-      "largeChartUrl": "",
+      "width": "100%",
+      "height": "100%",
       "noTimeScale": false,
-      "chartOnly": false
+      "hideDateRanges": false,
+      "hideMarketStatus": false,
+      "hideSymbolLogo": false
     }
     </script>
   </div>
